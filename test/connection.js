9c0e371fbdb44e19c1914b020bff547efd3c3145
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-11-17 17:54:02 +0530

    connect to server and fix deprecation warnings

---
+++ test/connection.js
@@ -0,0 +1,18 @@
+// Load the location of the database in variable
+require('dotenv').config()
+const uri = process.env.URI
+
+// Import mongoose to work with mongo
+const mongoose = require('mongoose')
+
+// Fix all deprecation warnings
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(uri)
+
+mongoose.connection
+    .once('open', () => console.log("Connection established"))
+    .on('error', (e) => console.log(e))
+
